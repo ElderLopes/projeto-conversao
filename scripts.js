7abbe494c49c7e28edd8f6ca073056d1e6c7e642
ElderLopes <elderfl85@hotmail.com>
2023-04-02 19:27:51 -0300

    Projeto de conversao

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const button = document.getElementById("convert-button")
+const select = document.getElementById("currency-select")
+const dolar = 5.06
+const euro = 5.49
+const bitcoin =0.0000071
+
+const convertValues = () => {
+    const inputReais = document.getElementById("input-real").value
+    const realValueText = document.getElementById("real-value-text")
+    const currencyValueText = document.getElementById("currency-value-text")
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: "BRL",
+    }).format(inputReais)
+    if (select.value === "US$ Dólar Americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-Us', {
+            style: 'currency',
+            currency: "USD",
+        }).format(inputReais / dolar)
+
+    }
+    if (select.value === "€ Euro") {
+
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: "EUR",
+        }).format(inputReais / euro)
+    }
+    if (select.value === "₿ Bitcoin") {
+
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-Us', {
+            style: 'currency',
+            currency: "BTC",
+        }).format(inputReais * bitcoin)
+    }
+
+}
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.getElementById("currency-img")
+
+    if (select.value === "US$ Dólar Americano") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./assetes/estados-unidos (1) 1.png"
+    }
+    if (select.value === "€ Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assetes/Euro.png"
+    }
+    if (select.value === "₿ Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assetes/BitCoin.png"
+    }
+   convertValues()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
